5179f41871e43ee931368029b3b2f230ca6a15a8
BruGeth <g15bru03@hotmail.com>
2025-10-07 20:55:01 -0500

    feat: refactorizar componente App para usar navegación y añadir imagen placeholder

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/screens/Home';
+import DetallePokemon from './src/screens/DetallePokemon';
+import Settings from './src/screens/Settings';
+import { SettingsProvider } from './src/context/SettingsContext';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <SettingsProvider>
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen name="Home" component={Home} />
+                    <Stack.Screen name="Detalle" component={DetallePokemon} />
+                    <Stack.Screen name="Settings" component={Settings} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </SettingsProvider>
+    );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
